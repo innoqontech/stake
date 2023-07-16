e79f884ab3a7dd9cefb76c5b9a5c219506b8976e
Ayushh Khare <ayushhkhare07092001@gmail.com>
2023-07-17 00:04:45 +0530

    Footer changer and styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ function Footer() {
                 <div className="col1">
                     <div>
                         <h3>Contact with us</h3>
-                        <p>connect@stack.com</p>
+                        <a>connect@stack.com</a>
                     </div>
                     <div>
                         <h3>Support</h3>
@@ -23,63 +23,93 @@ function Footer() {
                 </div>
                 <div className="col2">
                     <h3>Solutions</h3>
-                    <ul>
-                        <li>Enterprise</li>
-                        <li>Agencies</li>
-                        <li>Small Business</li>
-                        <li>Social Management</li>
-                        <li>Customer Care</li>
-                        <li>Industries</li>
-                        <li>Data & Intelligence</li>
-                    </ul>
+                    <div>
+                        <a>Enterprise</a>
+                        <br />
+                        <a>Agencies</a>
+                        <br />
+                        <a>Small Business</a>
+                        <br />
+                        <a>Social Management</a>
+                        <br />
+                        <a>Customer Care</a>
+                        <br />
+                        <a>Industries</a>
+                        <br />
+                        <a>Data & Intelligence</a>
+                    </div>
                 </div>
                 <div className="col3">
                     <h3>Platform</h3>
-                    <ul>
-                        <li>Analytics</li>
-                        <li>Engagement</li>
-                        <li>Publishing</li>
-                        <li>Business Operations</li>
-                        <li>Automation</li>
-                        <li>Team Management</li>
-                    </ul>
+                    <div>
+                        <a>Analytics</a>
+                        <br />
+                        <a>Engagement</a>
+                        <br />
+                        <a>Publishing</a>
+                        <br />
+                        <a>Business Operations</a>
+                        <br />
+                        <a>Automation</a>
+                        <br />
+                        <a>Team Management</a>
+                    </div>
                 </div>
                 <div className="col4">
                     <h3>Integrations</h3>
-                    <ul>
-                        <li>Facebook Management</li>
-                        <li>Twitter Management</li>
-                        <li>Instagram Management</li>
-                        <li>LinkedIN Management</li>
-                        <li>Pinterest Management</li>
-                        <li>Whats App Management</li>
-                        <li>Youtube Management</li>
-                        <li>All Integrations</li>
-                    </ul>
+                    <div>
+                        <a>Facebook Management</a>
+                        <br />
+                        <a>Twitter Management</a>
+                        <br />
+                        <a>Instagram Management</a>
+                        <br />
+                        <a>LinkedIN Management</a>
+                        <br />
+                        <a>Pinterest Management</a>
+                        <br />
+                        <a>Whats App Management</a>
+                        <br />
+                        <a>Youtube Management</a>
+                        <br />
+                        <a>All Integrations</a>
+                    </div>
                 </div>
                 <div className="col5">
                     <h3>Company</h3>
-                    <ul>
-                        <li>About Sprout</li>
-                        <li>Pricing</li>
-                        <li>Contact Us</li>
-                        <li>Careers</li>
-                        <li>Our Stories</li>
-                        <li>Investors</li>
-                        <li>Partners</li>
-                    </ul>
+                    <div>
+                        <a>About Sprout</a>
+                        <br />
+                        <a>Pricing</a>
+                        <br />
+                        <a>Contact Us</a>
+                        <br />
+                        <a>Careers</a>
+                        <br />
+                        <a>Our Stories</a>
+                        <br />
+                        <a>Investors</a>
+                        <br />
+                        <a>Partners</a>
+                    </div>
                 </div>
                 <div className="col6">
                     <h3>Resources</h3>
-                    <ul>
-                        <li>Blog</li>
-                        <li>Resource Center</li>
-                        <li>Social Media</li>
-                        <li>Agency Partner Directory</li>
-                        <li>Affiliate and Referrals</li>
-                        <li>Brand Assets</li>
-                        <li>Free Social Media Tools</li>
-                    </ul>
+                    <div>
+                        <a>Blog</a>
+                        <br />
+                        <a>Resource Center</a>
+                        <br />
+                        <a>Social Media</a>
+                        <br />
+                        <a>Agency Partner Directory</a>
+                        <br />
+                        <a>Affiliate and Referrals</a>
+                        <br />
+                        <a>Brand Assets</a>
+                        <br />
+                        <a>Free Social Media Tools</a>
+                    </div>
                 </div>
             </div>
         </div>
